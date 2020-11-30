1a07a4989e3f032ec8fb7ecb4b71765291cfd011
nadeplatelle <55435402+nadeplatelle@users.noreply.github.com>
2020-11-30 13:07:50 +0200

    Update firebase.js

--- breadline-update/src/firebase.js
+++ breadline-update/src/firebase.js
@@ -1,13 +1,4 @@
 import firebase from "firebase"
-const firebaseConfig = {
-    apiKey: "AIzaSyCanI-XWixO6tS7kiAr4CkAO7kNksQ1ZPE",
-    authDomain: "breadlinesa.firebaseapp.com",
-    databaseURL: "https://breadlinesa.firebaseio.com",
-    projectId: "breadlinesa",
-    storageBucket: "breadlinesa.appspot.com",
-    messagingSenderId: "832204674480",
-    appId: "1:832204674480:web:b8282d482cc09135a9f275"
-  };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
 
